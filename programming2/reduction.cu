083ad355b0424151f7b193eb05874d1af076d4c8
Guy Dickinson <guy@gdickinson.co.uk>
2012-03-22 00:50:04 -0400

    Added naive CUDA reduction. Known bug is for arrays > 1024 which is part D of the assignment.

--- programming2/reduction.cu
+++ programming2/reduction.cu
@@ -10,7 +10,7 @@ Copyright 2012 Guy Dickinson <guy.dickinson@nyu.edu>
 */
 
 // Vanilla, sequential reduction on host
-// length must be a multiple of 2.
+// This *would* have a divergence problem if it were multithreaded
 int findMaxOnHost(int* A, int length) {
     for (int step = 1; step < length; step *= 2) {
         for (int i = 0; i < length; i += (2 * step)) {
@@ -22,6 +22,44 @@ int findMaxOnHost(int* A, int length) {
     }
     return A[0];
 }
+
+__global__ void findMaxNaivelyKernel(int* A, int length) {
+    unsigned int tid = (blockIdx.x * blockDim.x) + threadIdx.x;
+    for (unsigned int step = 1; step < blockDim.x; step *= 2) {
+        __syncthreads();
+        if (tid % (2 * step) == 0) {
+            int candidate = A[tid + step];
+            if (A[tid] < candidate) {
+                A[tid] = candidate;
+            }
+        }
+        __syncthreads();
+    }
+    
+}
+
+// Naively find the maximum element, without taking into account thread
+// diversion or memory efficiency
+int cudaFindMaxOnDeviceNaively(int* A, int length) {
+    int size = length * sizeof(int);
+    int ret;
+    int* cudaArray;
+    cudaMalloc(&cudaArray, size);
+    cudaMemcpy(cudaArray, A, size, cudaMemcpyHostToDevice);
+    
+    dim3 dimBlock(1024 % length);
+    dim3 dimGrid(length / 1024, 1);
+    
+    findMaxNaivelyKernel<<<dimGrid, dimBlock>>>(cudaArray, length);
+    
+    // Recover just the first element from the device to save time.
+    cudaMemcpy(&ret, cudaArray, sizeof(int), cudaMemcpyDeviceToHost);
+    cudaFree(cudaArray);
+    return ret;
+}
+
+
+
 
 int* initializeArray(int length) {
     // Set up an array of ints of the right length
@@ -44,17 +82,35 @@ int* initializeArray(int length) {
 
 void runTest(int length) {
     timeval serialStart, serialEnd;
-    int* arr = initializeArray(length);
+    int* serialArr = initializeArray(length);
+    int* naiveParallelArray = initializeArray(length);
     
     gettimeofday(&serialStart, NULL);
-    int serialMax = findMaxOnHost(arr, length);
+    int serialMax = findMaxOnHost(serialArr, length);
     gettimeofday(&serialEnd, NULL);
+    free(serialArr);
+    
+    int expectedParallelMax = naiveParallelArray[length - 1];
+    int naiveParallelMax =
+        cudaFindMaxOnDeviceNaively(naiveParallelArray, length);
+    
+    
     
     // Make sure we actually found the max value
-    if (serialMax == arr[length - 1]) {
-        printf("Serial reduction passed check\n");
+    if (serialMax == serialArr[length - 1]) {
+        printf("Serial reduction passed check (expected %d, got %d)\n",
+            serialMax, serialArr[length - 1]);
     } else {
-        printf("Serial reduction failed!\n");
+        printf("Serial reduction failed! (expected %d, got %d)\n",
+            serialMax, serialArr[length - 1]);
+    }
+    
+    if (expectedParallelMax == naiveParallelArray[length - 1]) {
+        printf("Naive Parallel reduction passed check (expected %d, got %d)\n",
+            expectedParallelMax, naiveParallelArray[length - 1]);
+    } else {
+        printf("Naive Parallel reduction failed! (expected %d, got %d)\n",
+            expectedParallelMax, naiveParallelArray[length - 1]);
     }
     
     double serialElapsedTime =
@@ -63,6 +119,6 @@ void runTest(int length) {
 }
 
 int main(void) {
-    runTest(4096);
+    runTest(1024);
     return 0;
 }
